6910fa7f756f761e256168c007c07b55457c4ffa
Andrew Cauchi <drewcauchi@gmail.com>
2026-01-01 23:00:29 +0100

    feat: Installed API

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { Head, Link, usePage } from '@inertiajs/vue3';
-import { computed } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 
 const page = usePage()
 
 const device = computed(() => page.props.device)
+
+const fetchData = ref(null);
+
+onMounted(async () => {
+    const res = await fetch('http://leerdle.test/api/test');
+    fetchData.value = await res.json();
+});
 </script>
 
 <template>
@@ -16,5 +23,9 @@ const device = computed(() => page.props.device)
         <h2>Device -> IsMobile: {{ device?.is_mobile }}</h2>
         <h2>Device -> IsIos: {{ device?.is_ios }}</h2>
         <h2>Device -> IsAndroid: {{ device?.is_android }}</h2>
+
+        VALUE FROM API: {{ fetchData }}
+
+        test
     </div>
 </template>
